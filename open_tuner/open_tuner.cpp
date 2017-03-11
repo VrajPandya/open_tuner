418d762b036ef67a5299cb50e50e4c3b99252209
Vraj Pandya <vrajspandya@gmail.com>
2017-03-10 23:44:24 -0600

    start working on puthon

--- open_tuner/open_tuner.cpp
+++ open_tuner/open_tuner.cpp
@@ -11,17 +11,17 @@ int main(int argc, const char** argv){
     std::string commandArgs ("");
     std::string pythonCommand ("python ");
     std::string python3Command ("python3 ");
-    std::string openTunerHome ("" + OPEN_TUNER_ROOT);
+    std::string openTunerRoot (OPEN_TUNER_ROOT);
 
-    std::cout<<openTunerHome;
+    std::cout<<openTunerRoot<<"\n";
 
-    std::string openTunerStr ("open_tuner.py ");
+    std::string openTunerStr ("/open_tuner/open_tuner.py");
 
     for (int i = 1; i < argc ; ++i) {
         commandArgs = commandArgs + argv[i];
     }
 
-    std::string commandString = python3Command + openTunerStr +commandArgs;
+    std::string commandString = python3Command + openTunerRoot + openTunerStr + commandArgs;
     std::system(commandString.c_str());
 
     return 0;
